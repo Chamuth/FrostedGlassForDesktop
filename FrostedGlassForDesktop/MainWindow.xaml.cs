10e92694ee884b648fd266d168749a567ee239d7
Chamuth Chamandana <c.chamandana@gmail.com>
2017-07-02 11:08:58 +0530

    Initial commit

--- FrostedGlassForDesktop/MainWindow.xaml.cs
+++ FrostedGlassForDesktop/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,9 +11,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wallpaper;
 
 namespace FrostedGlassForDesktop
 {
@@ -23,6 +27,59 @@ namespace FrostedGlassForDesktop
         public MainWindow()
         {
             InitializeComponent();
+
+            var screenwidth = (SystemParameters.PrimaryScreenWidth);
+            var screenheight = (SystemParameters.PrimaryScreenHeight);
+
+            wallpaper_instance.Source = Screenshot.CopyScreen();
+            wallpaper_instance.Width = screenwidth;
+            wallpaper_instance.Height = screenheight;
+            RenderOptions.SetBitmapScalingMode(wallpaper_instance, BitmapScalingMode.LowQuality);
+
+            canvas.Children.Add(wallpaper_instance);
+
+            BlurBitmapEffect myBlurEffect = new BlurBitmapEffect();
+
+            // Set the Radius property of the blur. This determines how 
+            // blurry the effect will be. The larger the radius, the more
+            // blurring. 
+            myBlurEffect.Radius = 15;
+
+            // Set the KernelType property of the blur. A KernalType of "Box"
+            // creates less blur than the Gaussian kernal type.
+            myBlurEffect.KernelType = KernelType.Box;
+
+            // Apply the bitmap effect to the Button.
+            wallpaper_instance.BitmapEffect = myBlurEffect;
+        }
+
+
+        private Image wallpaper_instance = new Image();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+
+            UpdatePosition();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdatePosition();
+        }
+
+        private void UpdatePosition()
+        {
+            var left = Application.Current.MainWindow.Left;
+            var top = Application.Current.MainWindow.Top;
+
+            Canvas.SetLeft(wallpaper_instance, left * -1);
+            Canvas.SetTop(wallpaper_instance, top * -1);
+        }
+
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+            UpdatePosition();
         }
     }
 }
---
+++ FrostedGlassForDesktop/Screenshot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using System.Windows.Interop;
+using System.Windows;
+
+public class Screenshot
+{
+    public static BitmapSource CopyScreen()
+    {
+        var left = Screen.AllScreens.Min(screen => screen.Bounds.X);
+        var top = Screen.AllScreens.Min(screen => screen.Bounds.Y);
+        var right = Screen.AllScreens.Max(screen => screen.Bounds.X + screen.Bounds.Width);
+        var bottom = Screen.AllScreens.Max(screen => screen.Bounds.Y + screen.Bounds.Height);
+        var width = right - left;
+        var height = bottom - top;
+
+        using (var screenBmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+        {
+            using (var bmpGraphics = Graphics.FromImage(screenBmp))
+            {
+                bmpGraphics.CopyFromScreen(left, top, 0, 0, new System.Drawing.Size(width, height));
+                return Imaging.CreateBitmapSourceFromHBitmap(
+                    screenBmp.GetHbitmap(),
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+            }
+        }
+    }
+}
